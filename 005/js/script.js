e37fd55d2fd11449f830a09aa103fcea0035eed4
Bogdan <bks110497@gmail.com>
2020-07-29 14:06:57 +0300

    first commit

---
+++ 005/js/script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+a = 15;
+console.log(a);
+
+console.log(1);
+let number = 5;
+const leftBorderWidth = 1;
+
+leftBorderWidth = 10;
+
+number = 10;
+console.log(number);
+
+const obj = {
+    a: 50
+};
+
+obj.a = 10;
+console.log(obj);
---
+++ 006/script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+let number = 4;
+
+console.log(-4/0);
+console.log('string' * 9);
+
+const persone = `Alex`;
+
+const bool = false;
+
+console.log(something);
+
+let und;
+console.log(und);
+
+const obj = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+//console.log(obj.name);
+console.log(obj["name"]);
+
+          0             1            2   3            4   5
+let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+console.log(arr[2]);

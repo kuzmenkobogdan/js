59f269e3e56d646393bf734572b28349200ae331
Bogdan <bks110497@gmail.com>
2020-08-08 17:19:34 +0300

    дз_18

---
+++ 017/script.js
@@ -0,0 +1,25 @@
+// "use strict";
+
+// const str = "teSt";
+// const arr = [1, 2, 3];
+
+// console.log(str.toUpperCase());
+// console.log(str.toLocaleLowerCase());
+// console.log(str);
+
+// const fruit = "Some fruit";
+// console.log(fruit.indexOf("q"));
+
+// const logg = "Hello World";
+// console.log(logg.slice(6, 10));
+// // console.log(logg.slice(6));
+// // console.log(logg.slice(-5, -1));
+// console.log(logg.substring(6, 11));
+// console.log(logg.substr(6, 5)); // Последнее значение спрашивает сколько символов вырезать.
+
+// const num = 12.2;
+// console.log(Math.round(num));
+
+// const test = "12.2px";
+// console.log(parseInt(test));
+// console.log(parseFloat(test));
